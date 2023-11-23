6792ad0395092dcbf06c6988a3b50314a4df634f
Philip <ajibolaphilip10@gmail.com>
2023-11-22 18:39:47 -0800

    update change

--- WorldPopulationGrowth.java
+++ WorldPopulationGrowth.java
@@ -5,22 +5,25 @@ public class WorldPopulationGrowth{
    Scanner scr = new Scanner(System.in);
 
      System.out.print("Enter The World Current Population: ");
-           double currentPopulation = scr.nextDouble();
+           long currentPopulation = scr.nextLong();
 
       System.out.print("Enter The Annual Population Growth Rate(%): ");
-       double annualGrowthRate  = scr.nextDouble();
+       int annualGrowthRate  = scr.nextInt();
+       
+       double percentage = annualGrowthRate/100.0;
 
-       double populationAfterOneYear = currentPopulation + (currentPopulation*(annualGrowthRate/100));
-       double populationAfterTwoYear = populationAfterOneYear + (populationAfterOneYear*(annualGrowthRate/100));
-       double populationAfterThreeYear = populationAfterTwoYear + (populationAfterTwoYear*(annualGrowthRate/100));
-       double populationAfterFourYear = populationAfterThreeYear + (populationAfterThreeYear*(annualGrowthRate/100));
-       double populationAfterFiveYear = populationAfterFourYear + (populationAfterFourYear*(annualGrowthRate/100));
+
+       long populationAfterOneYear = currentPopulation +(long) (currentPopulation*(percentage));
+       long populationAfterTwoYear = populationAfterOneYear + (long)(populationAfterOneYear*(percentage));
+       long populationAfterThreeYear = populationAfterTwoYear +(long) (populationAfterTwoYear*(percentage));
+       long populationAfterFourYear = populationAfterThreeYear +(long) (populationAfterThreeYear*(percentage));
+       long populationAfterFiveYear = populationAfterFourYear +(long) (populationAfterFourYear*(percentage));
   
-       System.out.printf("The Estimate Of The Population Next Year is %.2f%n: ",populationAfterOneYear);
-        System.out.printf("The Estimate Of The Population Next Two Year is %.2f%n: ",populationAfterTwoYear);
-          System.out.printf("The Estimate Of The Population Next Three Year is %.2f%n: ",populationAfterThreeYear);
-         System.out.printf("The Estimate Of The Population Next Four Year is %.2f%n: ",populationAfterFourYear);
-       System.out.printf("The Estimate Of The Population Next Five Year is %.2f%n: ",populationAfterFiveYear);
+       System.out.println("The Estimate Of The Population Next Year is  "+populationAfterOneYear);
+        System.out.println("The Estimate Of The Population Next Two Year is  "+populationAfterTwoYear);
+          System.out.println("The Estimate Of The Population Next Three Year is  "+populationAfterThreeYear);
+         System.out.println("The Estimate Of The Population Next Four Year is  "+populationAfterFourYear);
+       System.out.println("The Estimate Of The Population Next Five Year is "+populationAfterFiveYear);
   }
 
 
